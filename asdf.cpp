4fb43c7cf9ee83e31ac74ffdec65b550355e65c2
dltmdcks98 <dltmdcks98@naver.com>
2021-07-05 15:37:24 +0900

    a

--- asdf.cpp
+++ asdf.cpp
@@ -1,16 +1,9 @@
 #include <iostream>
-#include <vector>
-#include <string>
 
-using namespace std;
 
-int main()
+
+int main(void)
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
+    std::cout<<"Hello World"<<std::endl;;
+    return 0;
 }

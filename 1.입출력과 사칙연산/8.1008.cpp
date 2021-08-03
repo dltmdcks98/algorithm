228926db8277109c77cffa7444a15ed0bb8dcf86
dltmdcks98 <dltmdcks98@naver.com>
2021-08-03 13:39:41 +0900

    8해결

--- 1.입출력과 사칙연산/8.1008.cpp
+++ 1.입출력과 사칙연산/8.1008.cpp
@@ -1,7 +1,13 @@
-#include <iostream>
-using namespace std;
-int main(){
-    int num1, num2;
-    cin >> num1>> num2;
-    cout << num1/num2 <<endl;
+#include<stdio.h>
+
+int main (int argc, char const *argv[]){
+    double a;
+    double b;
+    scanf("%lf %lf",&a , &b);
+
+    /*
+    lf출력은 소수점 6자리 까지 출력 => 소숫점 자리를 늘려야함
+    */
+   printf("%.13lf",a/b);
+   return 0;
 }
--- algorithm/algorithm/test.cpp
+++ algorithm/algorithm/test.cpp
@@ -1,7 +1,17 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
+
 int main() {
-    float num1, num2;
-    cin >> num1 >> num2;
-    cout << num1 / num2 << endl;
+    double a;
+    double b;
+
+    scanf("%lf %lf", &a, &b);	// double 입력은 %lf 
+
+    /*
+     기본적으로 lf 출력은 소수점 6자리까지 출력한다.
+     그렇기 때문에 소수점 자리수를 늘려주어야 한다.
+     오차범위가 10^-9 이하여야 하므로 여유있게 %.10lf 이상으로 해주자.
+    */
+    printf("%.13lf", a / b);	// 유효숫자가 13개 
+
+    return 0;
 }

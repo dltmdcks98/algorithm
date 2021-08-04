53076e89b0a1e8bc18d1e6e1169067b6a7cf2af9
dltmdcks98 <dltmdcks98@naver.com>
2021-08-04 22:22:11 +0900

    2if문 완료

--- 2.if문/4.2884.cpp
+++ 2.if문/4.2884.cpp
@@ -1,5 +1,16 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int H,M;
+    cin >> H >> M;
+    if(M < 45){
+        M += 15;
+        H--;
+    }else
+        M -= 45; 
+
+    if(H < 0)
+        H = 23;
     
+    cout << H <<" "<< M;
 }

b7f6aa9b313fa6ff2d3d26bc2eaf34bbf6abbe3a
dltmdcks98 <dltmdcks98@naver.com>
2021-08-07 18:22:16 +0900

    3-1

---
+++ 3.for문/1.2739.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cin >> a;
+
+    for(int b = 1; b<=9; b++){
+        cout<<a<<" * "<<b<<" = "<<a*b<<'\n';     
+    }
+}
+//*,= 사이의 띄어쓰기 유의

95864953a54a8e8ed492b153570689403945d314
dltmdcks98 <dltmdcks98@naver.com>
2021-09-05 19:26:58 +0900

    2742

--- algorithm/algorithm/test.cpp
+++ algorithm/algorithm/test.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
 using namespace std;
 int main() {
-    int n, a, b;
+    int n,m;
     cin >> n;
-    for (int i = 1; i <= n; i++) {
-        cin >> a >> b;
-        cout << a + b << "\n";
+    m = n;
+    for (int i = 0; i < n; i++) {
+        cout << m << "\n";
+        m--;
     }
 }

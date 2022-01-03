697492b9bfd8a17b6199c7e8b4ba5b398ef9f024
dltmdcks98 <dltmdcks98@gmail.com>
2022-01-03 17:25:03 +0900

    9

--- 3.for문/9.2438.cpp
+++ 3.for문/9.2438.cpp
@@ -1,9 +1,23 @@
-#include<iostream>
+#include <iostream>
+ 
 using namespace std;
-
-int main(){
-    int n;
-    for(int i = 0; i<n; i++ ){
-        
-    }
+ 
+int main(int argc, char const *argv[]) {
+	ios_base::sync_with_stdio(false); // 동기화 된 두 스트림을 끊어준다.
+ 
+	int N;
+	cin >> N;
+ 
+	// N번만큼 반복
+	for (int i = 1; i <= N; i++) {
+ 
+		// i행에서는 i개만큼 별을 출력
+		for (int j = 1; j <= i; j++) {
+			cout << "*";
+		}
+ 
+		// 해당 행이 출력이 끝났으면 반드시 줄바꿈을 해준다.
+		cout << "\n";
+	}
+	return 0;
 }

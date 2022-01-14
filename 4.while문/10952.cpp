68c6ffc0885ce42c418f45eed8972a0eb1de5eaf
dltmdcks98 <dltmdcks98@gmail.com>
2022-01-14 15:01:47 +0900

    10952

---
+++ 4.while문/10952.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int a,b,sum;
+    while (1)
+    {
+        cin >> a >> b;
+        if( a == 0 || b ==0){
+            break;            
+            }
+
+        sum = a + b;
+        cout << sum <<endl;
+    }
+    return 0;
+}

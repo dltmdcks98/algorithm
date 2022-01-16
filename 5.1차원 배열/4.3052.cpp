ddffbc126e14daa4f7f6b39b74cb55ee04bf7bba
dltmdcks98 <dltmdcks98@gmail.com>
2022-01-16 21:43:36 +0900

    3052

--- 5.1차원 배열/4.3052.cpp
+++ 5.1차원 배열/4.3052.cpp
@@ -1,5 +1,19 @@
 #include<iostream>
 using namespace std;
 int main (){
-    
-}
+    int arr[42]={};
+
+    int A;
+    for(int i = 0; i < 10; i++){
+        cin >> A;
+        arr[A % 42]++;
+    }
+
+    int res = 0;
+    for(int i : arr){
+        if(i > 0){
+            res++;
+        }
+    }
+    cout << res << endl;
+}   

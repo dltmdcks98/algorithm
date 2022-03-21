8d27ee60a6e1d9501e2bd666d7e4dc7c7d123aad
dltmdcks98 <dltmdcks98@gmail.com>
2022-03-21 18:30:53 +0900

    2675

---
+++ 7.문자열/4.2675.cpp
@@ -0,0 +1,19 @@
+#include<iostream>2
+using namespace std;
+int main(){
+    int num;
+    cin >> num;
+    for(int i = 0; i < num; i++){
+        int num2;
+        string arr;
+        cin >> num2;
+        cin >> arr;
+        for(int j = 0; j< arr.length(); j++){
+            for(int t = 0; t < num2;  t++){
+                cout << arr[j];
+            }
+        }
+        cout << endl;
+    }
+
+}
